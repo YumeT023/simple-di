456f8e8c61ec341ae3463385c3ae20451d4d0a6f
Yume <hei.hajatiana@gmail.com>
2023-04-17 16:08:46 +0300

    chore: initialize spring project

---
+++ src/main/java/com/example/simpledi/SimpleDiApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpledi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleDiApplication.class, args);
+  }
+
+}
